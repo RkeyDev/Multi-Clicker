b1a23f3019d49576bf463594ce45af34af1a0d00
Mr_Sir7777 <rkey671@gmail.com>
2024-08-25 12:29:19 +0300

    Feat: set target key button now works

--- src/App.java
+++ src/App.java
@@ -13,22 +13,34 @@ public class App extends Application{
 
     @Override
     public void start(Stage main_stage) throws Exception {
+        //Load the scene from the fxml file
         FXMLLoader scene_loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
-        Parent root = scene_loader.load();        
+        Parent root = scene_loader.load(); 
 
         MainController controller = new MainController();
         Scene scene = new Scene(root);
         
         
         scene.setOnKeyPressed(event -> { //Listen for keys the user pressed
+
+            //TODO merge the if statements together
+
             if(controller.getIsSettingHotkey()){
                 controller.setHotkey(event.getCode().toString());
-                controller.setIsSettingHotkey(false);;
+                if (!controller.getHotkey().equals(controller.getTargetedKey())) {
+                    controller.setIsSettingHotkey(false);
+                }
+            }else if(controller.getIsSettingTargetKey()){
+                controller.setTargetedKey(event.getCode().toString());
+                if (!controller.getHotkey().equals(controller.getTargetedKey())) {
+                    controller.setIsSettingTargetKey(false);
+                }
             }
         });
 
         main_stage.setTitle("Multi-Clicker");
         main_stage.setScene(scene);
+        main_stage.setResizable(false);
         
         main_stage.show(); 
     }
--- src/MainController.java
+++ src/MainController.java
@@ -7,14 +7,21 @@ import javafx.scene.text.Text;
 public class MainController {
 
     static boolean is_setting_hotkey = false;
-    static String start_hotkey = "F6";
+    static boolean is_setting_target_key = false;
+    static String hotkey = "F6";
+    static String targeted_key = "SPACE";
+
+    Thread changeHotkeyLabel;
+    Thread changeTargetedKeyLabel;
 
     @FXML private Button hotkey_button;
     @FXML private Button targeted_key_button;
     @FXML private Text hotkey_label;
+    @FXML private Text targeted_key_label;
     @FXML private TextField cooldown_text_field;
 
-    Thread changeHotkeyLabel;
+    
+    //TODO Merge setNewHotKey and setTargetKey together
 
     @FXML
     public void setNewHotkey(ActionEvent event){
@@ -24,7 +31,7 @@ public class MainController {
             
             changeHotkeyLabel = new Thread(()->{
                 while (is_setting_hotkey){
-                    this.hotkey_label.setText(start_hotkey.toUpperCase()); //Set the hotkey label to the current hotkey
+                    this.hotkey_label.setText(hotkey.toUpperCase()); //Set the hotkey label to the current hotkey
                 }
 
                 enableAllUiComponents(); //Enable the UI components 
@@ -34,12 +41,32 @@ public class MainController {
         }
     }
 
+    @FXML
+    public void setTargetedKey(ActionEvent event){
+        if (is_setting_target_key == false) {
+            disableAllUiComponents(); //Disable the UI components
+            is_setting_target_key = true;
+            
+            changeTargetedKeyLabel = new Thread(()->{
+                while (is_setting_target_key){
+                    this.targeted_key_label.setText(targeted_key.toUpperCase()); //Set the hotkey label to the current hotkey
+                }
+
+                enableAllUiComponents(); //Enable the UI components 
+            });
+
+            changeTargetedKeyLabel.start();
+        }
+    }
+
+
     
     public void disableAllUiComponents(){
         //Disable all UI components
 
         this.cooldown_text_field.setDisable(true);
         this.targeted_key_button.setDisable(true);
+        this.hotkey_button.setDisable(true);
     }
 
     public void enableAllUiComponents(){
@@ -47,18 +74,23 @@ public class MainController {
 
         this.cooldown_text_field.setDisable(false);
         this.targeted_key_button.setDisable(false);
+        this.hotkey_button.setDisable(false);
     }
 
 
 
 
 
-    public void setHotkey(String hotkey){start_hotkey = hotkey;}
+    public void setHotkey(String key) {hotkey = key;}
+    public void setTargetedKey(String key) {targeted_key = key;}
+
 
     public void setIsSettingHotkey(boolean isSettingHotkey) {is_setting_hotkey = isSettingHotkey;}
+    public void setIsSettingTargetKey(boolean isSettingTargetKey) {is_setting_target_key = isSettingTargetKey;}
 
+    public boolean getIsSettingTargetKey() {return is_setting_target_key;}
     public boolean getIsSettingHotkey() {return is_setting_hotkey;}
-
-
+    public String getHotkey() {return hotkey;}
+    public String getTargetedKey() {return targeted_key;}
     
 }
